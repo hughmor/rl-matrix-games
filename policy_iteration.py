972da512cda0111f7e15b7be075d07158250c75f
Hugh Morison <hugh.morison@hotmail.ca>
2019-10-25 14:53:56 -0400

    refactor

--- policy_iteration.py
+++ policy_iteration.py
@@ -1,25 +1,25 @@
 import numpy as np
 
-p_d = np.array([[5, 0],
-                [10, 1]])
-m_c = np.array([[1, -1],
-                [-1, 1]])
-r_p_s = np.array([[0, -1, 1],
-                  [1, 0, -1],
-                  [-1, 1, 0]])
-p1_rewards = [p_d, m_c, r_p_s]
-p2_rewards = [p_d.transpose(), -m_c, -r_p_s]
+prisoners_dilemma = np.array([[5, 0],
+                              [10, 1]])
+matching_coins = np.array([[1, -1],
+                           [-1, 1]])
+rock_paper_scissors = np.array([[0, -1, 1],
+                                [1, 0, -1],
+                                [-1, 1, 0]])
+p1_rewards = [prisoners_dilemma, matching_coins, rock_paper_scissors]
+p2_rewards = [p1_rewards[0].transpose(), -p1_rewards[1], -p1_rewards[2]]
 
 
 def normalize(policy):
     if (policy < 0).any():
-        policy -= policy.min() # if any arg is negative raise all values so minimum is 0
+        raise ValueError('Policy has negative value')
     tot = policy.sum()
     return policy/tot
 
 
-def play_game(reward_matrix1, reward_matrix2, policy1, policy2):
-    assert len(reward_matrix1) == len(reward_matrix2) == len(policy1) == len(policy2)
+def play_game(policy1, reward_matrix1, policy2, reward_matrix2):
+    assert len(reward_matrix1) == len(reward_matrix2) == len(policy1) == len(policy2), 'Size of policy arrays and rewards matrices do not match'
     n = len(reward_matrix1)
     p1_action = np.random.choice(n, p=policy1)
     p2_action = np.random.choice(n, p=policy2)
@@ -28,33 +28,36 @@ def play_game(reward_matrix1, reward_matrix2, policy1, policy2):
     return (p1_action, p1_reward), (p2_action, p2_reward)
 
 
-def update_policy(p, a, r, ALPHA, mode=1):
+def update_policy(policy, action, reward, learning_rate, mode=1):
     if mode == 1:
-        p += -ALPHA * r * p
+        policy += -learning_rate * reward * policy
     else:
-        p += -ALPHA * r * p + ALPHA*(E-p[a])    # NOT IMPLEMENTED
-    p[a] += ALPHA * r
-    return normalize(p)
+        raise NotImplementedError('Havent implemented expected value (E)')
+        policy += -learning_rate * reward * policy + learning_rate*(E-policy[action])
+    policy[action] += learning_rate * reward
+    return normalize(policy)
 
 
-def iterate(p1, p2, matrix1, matrix2, ALPHA=0.005, T=500000, alg=1):
+def iterate(policy1, reward_matrix1, policy2, reward_matrix2, alpha=0.005, max_iterations=100000, mode=1):
     p1_history = []
     p2_history = []
-    for t in range(T):
-        p1_history.append([*p1])
-        p2_history.append([*p2])
-        (a1, r1), (a2, r2) = play_game(matrix1, matrix2, p1, p2)
-        p1 = update_policy(p1, a1, r1, ALPHA, mode=alg)
-        p2 = update_policy(p2, a2, r2, ALPHA, mode=alg)
+    done_iterating = False
+    for t in range(max_iterations):
+        p1_history.append([*policy1])
+        p2_history.append([*policy2])
+        (a1, r1), (a2, r2) = play_game(policy1, reward_matrix1, policy2, reward_matrix2)
+        policy1 = update_policy(policy1, a1, r1, learning_rate=alpha, mode=mode)
+        policy2 = update_policy(policy2, a2, r2, learning_rate=alpha, mode=mode)
+        if done_iterating:
+            break
     return p1_history, p2_history
 
 
 if __name__ == '__main__':
-    p1_r = p1_rewards[0]
-    p2_r = p2_rewards[0]
+    matrix_1 = p1_rewards[0]
+    matrix_2 = p2_rewards[0]
 
     policy_1 = normalize(np.random.random(2))
     policy_2 = normalize(np.random.random(2))
 
-    p1_history, p2_history = iterate(policy_1, policy_2, p1_r, p2_r)
-
+    p1_history, p2_history = iterate(policy_1, matrix_1, policy_2, matrix_2)

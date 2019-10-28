91f86f0f9734d0dc816401517b4fd2171581b5f4
Matthew Filipovich <mjfilipovi@gmail.com>
2019-10-28 19:03:08 -0400

    Move functions to policy_iteration.py. Change learning rate value.

--- policy_iteration.py
+++ policy_iteration.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 prisoners_dilemma = np.array([[5, 0],
                               [10, 1]])
@@ -9,49 +12,104 @@ rock_paper_scissors = np.array([[0, -1, 1],
                                 [-1, 1, 0]])
 p1_rewards = [prisoners_dilemma, matching_coins, rock_paper_scissors]
 p2_rewards = [p1_rewards[0].transpose(), -p1_rewards[1], -p1_rewards[2]]
+action_labels = [['lie', 'confess'],
+                 ['head', 'tail'],
+                 ['rock', 'paper', 'scissors']]
+games = ['prisoners dilemma', 'matching pennies', 'rock paper scissors']
 
 
 def normalize(policy):
     if (policy < 0).any():
         raise ValueError('Policy has negative value')
-    tot = policy.sum()
-    return policy/tot
+    total = policy.sum()
+    return policy/total
 
 
-def play_game(policy1, reward_matrix1, policy2, reward_matrix2):
-    assert len(reward_matrix1) == len(reward_matrix2) == len(policy1) == len(policy2), \
-        'Size of policy arrays and rewards matrices do not match'
-    n = len(reward_matrix1)
-    p1_action = np.random.choice(n, p=policy1)
-    p2_action = np.random.choice(n, p=policy2)
-    p1_reward = reward_matrix1[p1_action, p2_action]
-    p2_reward = reward_matrix2[p1_action, p2_action]
-    return (p1_action, p1_reward), (p2_action, p2_reward)
+def play_game(policies, rewards):
+    n = len(rewards[0])
+    p1_action = np.random.choice(n, p=policies[0])
+    p2_action = np.random.choice(n, p=policies[1])
+    p1_reward = rewards[0][p1_action, p2_action]
+    p2_reward = rewards[1][p1_action, p2_action]
+    return (p1_action, p2_action), (p1_reward, p2_reward)
 
 
-def update_policy(policy, action, reward, learning_rate, mode=1):
-    if mode == 1:
-        policy += -learning_rate * reward * policy
+def update_policy(policy, action_taken, reward, learning_rate=0.005, expected_values=None, update_alg='standard', N=None):
+    if expected_values is None:
+        expected_values = [0 for _ in range(len(policy))]
+    for action in range(len(policy)):
+        if action is not action_taken:
+            if update_alg == 'standard':
+                policy[action] += -learning_rate * reward * policy[action]
+            elif update_alg == 'modified':
+                policy[action] += -learning_rate * reward * policy[action] + learning_rate * (expected_values[action] - policy[action])
+                expected_values[action] += learning_rate * (policy[action]-expected_values[action])
+            else:
+                raise ValueError('Invalid value supplied to update_alg')
+        else:
+            if update_alg == 'standard':
+                policy[action] += learning_rate * reward * (1-policy[action])
+            elif update_alg == 'modified':
+                policy[action] += learning_rate * reward * (1-policy[action]) + learning_rate * (expected_values[action] - policy[action])
+                expected_values[action] += learning_rate * (policy[action]-expected_values[action])
+            else:
+                raise ValueError('Invalid value supplied to update_alg')
+    return normalize(policy), expected_values
+
+
+def iterate(policy1, reward_matrix1, policy2, reward_matrix2, alpha=0.005, max_iterations=10000, update_alg='standard'):
+    p_history = [[policy1],[policy2]]
+    policies = [policy1, policy2]
+    rewards = [reward_matrix1, reward_matrix2]
+    expected_values = [[0 for _ in range(len(policy1))]] * 2
+    Q = [[0 for _ in range(len(policy1))]] * 2
+    N = [[0 for _ in range(len(policy1))]] * 2     
+    for _ in range(max_iterations):
+        action, reward = play_game(policies, rewards)
+        for i in range(2):
+            N[i][action[i]] += 1
+            Q[i][action[i]] += 1/N[i][action[i]]*(reward[i]-Q[i][action[i]])
+            policies[i], expected_values[i] = update_policy(policies[i], action[i], reward[i], learning_rate=alpha, expected_values=expected_values[i], update_alg=update_alg, N=N[i][action[i]])
+            p_history[i].append(policies[i])
+    return p_history[0], p_history[1], Q[0], Q[1]
+
+
+def make_plots(player1_history, player2_history, action_labels):
+    episodes = [n/1000.0 for n in range(len(player1_history))]
+    players_list = [zip(*player1_history), zip(*player2_history)]
+    figure = plt.figure()
+    subplots = []
+    subplots.append(figure.add_subplot(121))
+    subplots.append(figure.add_subplot(122, sharey=subplots[0]))
+    for subplot, player, i in zip(subplots, players_list, range(2)):
+        for policy, action in zip(player, action_labels):
+            subplot.plot(episodes, policy, label=action)
+        subplot.set_xlabel('Episode ($*10^3$)')
+        subplot.set_ylim([0,1])
+        subplot.set_title('Player ' + str(i+1))
+        subplot.legend()
+    figure.text(0.06, 0.5, 'Action Probability', ha='center', va='center', rotation='vertical')
+    
+def make_trajectory_plots(player1_history, player2_history, action_labels):
+    if len(action_labels) == 2:
+        figure = plt.figure()
+        raise NotImplementedError
+    elif len(action_labels) == 3:
+        p1_hist = list(zip(*player1_history))
+        p2_hist = list(zip(*player2_history))
+        figure = plt.figure()
+        ax3d = figure.add_subplot('111',projection='3d')
+        ax3d.plot(p1_hist[0],p1_hist[1],p1_hist[2],'C1',lw=0.85)
+        ax3d.scatter(p1_hist[0][-1],p1_hist[1][-1],p1_hist[2][-1],s=5,c='C1')
+        ax3d.plot(p2_hist[0],p2_hist[1],p2_hist[2],'C2',lw=0.85)
+        ax3d.scatter(p2_hist[0][-1],p2_hist[1][-1],p2_hist[2][-1],s=5,c='C2')
+        ax3d.set_title('Policy Trajectory'.format())
+        ax3d.set_xlabel('$P_{rock}$')
+        ax3d.set_ylabel('$P_{paper}$')
+        ax3d.set_zlabel('$P_{scissors}$')
     else:
-        raise NotImplementedError('Havent implemented expected value (E)')
-        policy += -learning_rate * reward * policy + learning_rate*(E-policy[action])
-    policy[action] += learning_rate * reward
-    return normalize(policy)
-
-
-def iterate(policy1, reward_matrix1, policy2, reward_matrix2, alpha=0.005, max_iterations=100000, mode=1):
-    p1_history = []
-    p2_history = []
-    done_iterating = False
-    for t in range(max_iterations):
-        p1_history.append([*policy1])
-        p2_history.append([*policy2])
-        (a1, r1), (a2, r2) = play_game(policy1, reward_matrix1, policy2, reward_matrix2)
-        policy1 = update_policy(policy1, a1, r1, learning_rate=alpha, mode=mode)
-        policy2 = update_policy(policy2, a2, r2, learning_rate=alpha, mode=mode) # Doesn't capture final policy in history?
-        if done_iterating:
-            break
-    return p1_history, p2_history
+        raise NotImplementedError
+    
 
 
 if __name__ == '__main__':
